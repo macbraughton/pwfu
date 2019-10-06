8d7d355e30730a19bf024599d9b5305a6aa8417c
Donnie Hicks <dw@1hx.ca>
2019-10-05 21:10:15 -0400

    added some --help info

--- crypto.js
+++ crypto.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 require('dotenv').config()
 const commandLineArgs = require('command-line-args')
+const commandLineUsage = require('command-line-usage')
 const crypto = require("crypto")
 const secret = process.env.SECRET
+
 const optionDefinitions = [
-  { name: 'random', alias: 'r', type: Boolean },
   { name: 'algorithm', alias: 'a', type: String },
   { name: 'encoding', alias: 'e', type: String },
   { name: 'src', type: String, multiple: true, defaultOption: true },
-  { name: 'hashes', alias: 'l', type: Boolean }
+  { name: 'hashes', alias: 'l', type: Boolean },
+  { name: 'help', alias: 'h', type: Boolean }
 ]
 
 const options = commandLineArgs(optionDefinitions)
@@ -21,6 +23,45 @@ if (options.hashes) {
   }
   console.log(formatHashes(crypto.getHashes()))
 } 
+else if (options.help) {
+  const sections = [
+    {
+      header: 'pwfu',
+      content: 'Generates random and deterministic passwords'
+    },
+    {
+      header: 'Options',
+      optionList: [
+        {
+          name: 'src',
+          typeLabel: '{underline string}',
+          description: 'Seed for hashing algorithm'
+        },
+        {
+          name: 'algorithm',
+          alias: 'a',
+          description: 'Hashing algorithm (default: whirlpool)',
+        },
+        {
+          name: 'encoding',
+          alias: 'e',
+          description: 'Output encoding (default: base64)'
+        },
+        {
+          name: 'hashes',
+          alias: 'l',
+          description: 'Print available hashing algorithms'
+        },
+        {
+          name: 'help',
+          description: 'Print this usage guide.'
+        }
+      ]
+    }
+  ]
+  const usage = commandLineUsage(sections)
+  console.log(usage)
+}
 else {
   options.src = options.src 
     ? options.src.join("") 
