0ad8af02ca37dfee2fbc1255bdc9bf25c82afe41
Donnie Hicks <dw@1hx.ca>
2019-10-05 20:44:28 -0400

    reorganized for cleaner logic

--- crypto.js
+++ crypto.js
@@ -9,25 +9,26 @@ const optionDefinitions = [
   { name: 'encoding', alias: 'e', type: String },
   { name: 'src', type: String, multiple: true, defaultOption: true },
   { name: 'hashes', alias: 'l', type: Boolean }
-  //{ name: 'timeout', alias: 't', type: Number }
 ]
 
 const options = commandLineArgs(optionDefinitions)
 
-options.src = options.src 
-  ? options.src.join("") 
-  : String(Math.exp(Math.log2(Math.random() * Date.now())))
-
-let hash = crypto.createHmac(options.hasher || 'whirlpool', secret)
-      .update(options.src)
-      .digest(options.encoding || 'base64')
-
-!options.hashes && console.log(hash.slice(0, 30))
-
-const formatHashes = hashes => {
-  let heading = "Available Hashing Algorithms:\n\t"
-  let formattedHashes = hashes.join("\n\t")
-  return heading + formattedHashes
+if (options.hashes) {
+  const formatHashes = hashes => {
+    let heading = "Available Hashing Algorithms:\n\t"
+    let formattedHashes = hashes.join("\n\t")
+    return heading + formattedHashes
+  }
+  console.log(formatHashes(crypto.getHashes()))
+} 
+else {
+  options.src = options.src 
+    ? options.src.join("") 
+    : String(Math.exp(Math.log2(Math.random() * Date.now())))
+  
+  let hash = crypto.createHmac(options.hasher || 'whirlpool', secret)
+        .update(options.src)
+        .digest(options.encoding || 'base64')
+  
+  console.log(hash.slice(0, 30))
 }
-
-if (options.hashes) console.log(formatHashes(crypto.getHashes()))
